4000e9022824b872a30d7f19c6159bfa87544f45
pegeOS <pg.santana11@gmail.com>
2026-06-02 17:56:35 -0300

    Criando a Row View dos jogos

---
+++ FinalProjectFoundation/GameRowView.swift
@@ -0,0 +1,69 @@
+//
+//  GameRowView.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 02/06/26.
+//
+
+import SwiftUI
+
+struct GameRowView: View {
+    
+    var game : Game
+    var contador : Int = 0
+    
+    var body: some View {
+        HStack(alignment: .top){
+            GameCoverView(cover: UIImage(resource: .sla).pngData())
+                .frame(width:130, height:130)
+            
+            VStack(alignment: .leading){
+                Text(game.name).font(.system(size:32))
+                Spacer()
+                
+                // - - - - ESTRELAS - - - -
+                
+                HStack{
+                    
+                    ForEach(0..<game.star){ star in
+                        ZStack {
+                            Image(systemName: "star.fill")
+                                .foregroundStyle(.yellow)
+                            Image(systemName: "star")
+                                .foregroundStyle(.black)
+                        }
+                    }
+                    ForEach(0..<5){ index in
+                        if(index >= game.star){
+                            Image(systemName: "star")
+                        }
+                    }
+                }
+                // - - - - - - - - - - -
+                
+                Spacer()
+                HStack{
+                    Text("( " + String(game.numberRatings) + " Avaliações )")
+                    
+                    Button(action:{print("Botao clicado")}){
+                        Text(game.subgenre.name)
+                            .padding(4)
+                    }
+                    .background(Color.purple.opacity(0.6))
+                    .foregroundColor(.white)
+                    .cornerRadius(25)
+                    .frame(maxHeight: .infinity, alignment: .center)
+                }
+                Spacer()
+            }
+            .fixedSize(horizontal: false, vertical: true)
+        }
+    }
+//      .frame(maxWidth: .infinity, alignment: .leading)
+//      .frame(height: 100)
+}
+
+
+#Preview{
+    GameRowView(game: Game(id: 1, name: "Balatro", star:2, numberRatings: 1902, cover:UIImage(resource: .sla).pngData(), subgenre: SubGenre(id:1,name:"Roguelike")))
+}
---
+++ FinalProjectFoundation/Models.swift
@@ -0,0 +1,55 @@
+//  Models.swift
+//  FinalProjectFoundation
+//  Created by Found on 02/06/26.
+
+
+
+
+
+
+import Foundation
+import SwiftData
+import SwiftDataSQLite
+
+@SQLiteTable("games")
+//fazendo o @Model para transformar uma classe comum do swift em um modelo de dados gerenciado pelo SwiftData
+@Model
+class Game{//criando classe de Jogo
+
+    
+    //id, nome, numero de estrelas e subgenero, que sera vinculado a variavel "games" da classe SubGenre
+    var id: Int
+    var name: String
+    var star: Int
+    var cover: Data?
+    var numberRatings: Int
+    @Relationship(inverse: \SubGenre.games) var subgenre: SubGenre
+    
+    //inicializando as variaveis
+    init(id: Int = Int.random(in: 0...100), name: String, star: Int, numberRatings: Int, cover: Data?, subgenre: SubGenre){
+        
+        self.id = id
+        self.name = name
+        self.star = star
+        self.numberRatings = numberRatings
+        self.cover = cover
+        self.subgenre = subgenre
+    }
+}
+
+@SQLiteTable("subgenres")
+@Model
+class SubGenre{ // criando classe de subgenero de jogo
+    
+    //id, nome e array de jogos que nao precisa ser inicializado pois está vinculado a variavel "subgenre" da classe Game
+    var id: Int
+    var name: String
+    @Relationship var games: [Game] = []
+    
+    //inicializando as variaveis
+    init(id: Int = Int.random(in: 0...100), name: String){
+        
+        self.id = id
+        self.name = name
+    }
+}
