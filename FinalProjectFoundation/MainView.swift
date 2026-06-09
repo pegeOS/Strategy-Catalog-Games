bf0b1626acbe2aa68f131cef861bb4e3a59bae45
pegeOS <pg.santana11@gmail.com>
2026-06-09 15:38:43 -0300

    fazendo background e implementando a row view na main view

--- FinalProjectFoundation/MainView.swift
+++ FinalProjectFoundation/MainView.swift
@@ -10,8 +10,19 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         NavigationStack{
-            VStack{}
-            .navigationTitle("EXPLORAR")
+            VStack{
+                GameRowView(game: Game(id: 1, name: "Balatro", star:2, cover:UIImage(resource: .sla).pngData(), numberRatings: 1902, subgenre: SubGenre(id:1,name:"Roguelike")))
+                GameRowView(game: Game(id: 1, name: "Balatro", star:2, cover:UIImage(resource: .sla).pngData(), numberRatings: 1902, subgenre: SubGenre(id:1,name:"Roguelike")))
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(LinearGradient(
+                colors: [.roxao, .preto],
+                startPoint: .top,
+                endPoint: .bottom))
+            
+            .navigationTitle("Explorar")
+            .toolbarColorScheme(.dark, for: .navigationBar)
+            
         }
     }
 }
